7ea595c9068560127a58a83aaedfaa04c3576c2e
defaultpfp <107557004+defaultpfp@users.noreply.github.com>
2022-07-31 10:17:09 +0530

    Added license notice.

--- src/pypass.py
+++ src/pypass.py
@@ -1,3 +1,19 @@
+#    PyTenPass A non-interactive  read only Keepass compataible password manager.
+#    Copyright 2022 DefaultPFP
+
+#    PyTenPass is free software: you can redistribute it and/or
+#    modify it under the terms of the GNU General Public License as published
+#    by the Free Software Foundation, either version 3 of the License, or (at
+#    your option) any later version.
+#
+#    PyTenPass is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General
+#    Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License along
+#    with the PyTenPass.  If not, see <http://www.gnu.org/licenses/>.
+
 import argparse
 from pykeepass import PyKeePass
 parser = argparse.ArgumentParser()
